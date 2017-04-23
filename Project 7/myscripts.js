72632f95a19e165aaa195e1d36fc5a367ebfab68
Jason Figueroa <jason.figueroa2@gmail.com>
2017-04-23 19:40:37 +0000

    ready to validate

--- Project 7/myscripts.js
+++ Project 7/myscripts.js
@@ -1,13 +1,27 @@
 function init() {
 	var url = document.URL;
 	var filename = url.substring(url.lastIndexOf('/')+1);
-	
+
 	if(filename == "reservations.html") {
 		var btn = document.getElementById('test');
 		btn.style.background = '#90C7E3';
 		console.log("in if statement for reservations.html");
 	} else {
 		console.log("in else statement, filename is not reservations.html");
+	}
+	
+	if(filename == "accommodations.html" || filename == "activities.html") {
+		console.log("in if for jquery");
+		$('table tr:not(:first-child)').mouseover(function() { 
+			//$(this).css("background-color", "pink");
+			$(this).addClass('highlight-table-row'); 
+		} );
+		$('table tr:not(:first-child)').mouseout(function() { 
+			//$(this).css("background-color", "white"); 
+			$(this).removeClass('highlight-table-row');
+		} );
+		//$('table tr:not(:first-child)').css("background-color", "pink");
+		//$("tbody > ul > li:not(:first)").css("background-color", "pink");
 	}
     	
     	setInterval(function() {
